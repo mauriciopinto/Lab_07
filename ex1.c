97795218a7b3ed0e5edfde060e3d1531e32915c7
Mauricio <mauricio.pinto@utec.edu.pe>
2020-06-18 17:09:24 -0500

    random rw

--- ex1.c
+++ ex1.c
@@ -4,20 +4,43 @@
 #include <unistd.h>
 #include <limits.h>
 
-#define N 1288490188
+#define N 850000000
 #define MINUTES 1
 
 int main (void) {
-    //sleep (60 * MINUTES);
-    int *buffer = (int *) malloc (sizeof (int) * N);
+    sleep (60 * MINUTES);
+    long *buffer_1 = (long *) malloc (sizeof (long) * N);
+    long *buffer_2 = (long *) malloc (sizeof (long) * N);
+    long *buffer_3 = (long *) malloc (sizeof (long) * N);
+    long *buffer_4 = (long *) malloc (sizeof (long) * N);
+    
     srand(time(NULL));
 
     time_t start_time = time(NULL);
-
+    long i, b;
+    long j;
     while (time(NULL) - start_time < MINUTES * 60) {
-        int i = rand () % N;
-    	int j = buffer[i];
+	b = rand () % 4 + 1;
+	i = rand () % N;
+	switch (b) {
+		case 1: ;
+			j += buffer_1[i];
+			break;
+		case 2: ;
+                        j += buffer_2[i];
+                        break;
+		case 3: ;
+                        j += buffer_3[i];
+                        break;
+		case 4: ;
+                        j += buffer_4[i];
+                        break;
+	}
     }
+    free (buffer_1);
+    free (buffer_2);
+    free (buffer_3);
+    free (buffer_4);
 
     return 0;
 }
--- ex2.c
+++ ex2.c
@@ -3,14 +3,15 @@
 #include <time.h>
 #include <string.h>
 
-#define FILE_SIZE 1073741824
+#define FILE_SIZE 1288490188
 #define TWO_MB 2097152
 #define FIVE_MB 5242880
 
 void random_read (FILE *fp) {
 	char *read_buf = (char *) malloc (sizeof (char) * TWO_MB);
 	fseek (fp, rand () % FILE_SIZE - TWO_MB, 0);
-	fread (read_buf, FIVE_MB, 1, fp);
+	fread (read_buf, TWO_MB, 1, fp);
+	free (read_buf);
 }
 
 void random_write (FILE *fp) {
@@ -24,6 +25,7 @@ void sequential_read (FILE *fp) {
 	char *read_buf = (char *) malloc (sizeof (char) * TWO_MB);
         fseek (fp, rand () % FILE_SIZE - TWO_MB, 0);
         fread (read_buf, TWO_MB, 1, fp);
+	free (read_buf);
 }
 
 void sequential_write (FILE *fp) {
@@ -32,11 +34,23 @@ void sequential_write (FILE *fp) {
         fwrite ((void *) &c, 1, TWO_MB, fp);
 } 
 
+void random_rw (FILE *fp) {
+	char *read_buf = (char *) malloc (sizeof (char) * TWO_MB);
+	char *alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        char c = alpha[rand () % strlen (alpha)];
+	int i = rand () % 2;
+	if (i == 0)
+		fread (read_buf, FIVE_MB, 1, fp);
+	else 
+		fwrite ((void *) &c, 1, FIVE_MB, fp);
+        free (read_buf);
+}
+
 int main () {
 	time_t start, end;
 	srand (time (NULL));
 	char *filename = "sample.txt";
-	long int t = 5;
+	long int t = 2;
 	long int elapsed;
 	FILE *fp = fopen (filename, "rb+");
 
@@ -47,7 +61,7 @@ int main () {
 		elapsed = (end - start);
 	} while (elapsed < t);
 	
-	printf ("finished random reads\n");
+	//printf ("finished random reads\n");
 	start = time (NULL);
 	do {
 		random_write (fp);
@@ -55,7 +69,7 @@ int main () {
 		elapsed = (end - start);
 	} while (elapsed < t);
 
-	printf ("finished random writes\n");
+	//printf ("finished random writes\n");
 	fseek (fp, 0, 0);
 	start = time (NULL);
 	do {
@@ -64,7 +78,7 @@ int main () {
 		elapsed = (end - start);	
 	} while (elapsed < t);
 
-	printf ("finished sequential reads\n");
+	//printf ("finished sequential reads\n");
 	fseek (fp, 0, 0);
 	start = time (NULL);
         do {
@@ -72,6 +86,12 @@ int main () {
                 end = time (NULL);
 		elapsed = (end - start);
         } while (elapsed < t);
-	printf ("finished sequential writes\n");
+	//printf ("finished sequential writes\n");
+	
+	do {
+		random_rw (fp);
+		end = time (NULL);
+		elapsed = (end - start);
+	} while (elapsed < t);
 	fclose (fp);
 }
