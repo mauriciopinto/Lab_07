12e2e36da317408a5f7c88a52b3075ece1f6f146
Mauricio <mauricio.pinto@utec.edu.pe>
2020-06-17 21:45:38 -0500

    ex1

--- ex1.c
+++ ex1.c
@@ -2,19 +2,20 @@
 #include <time.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <limits.h>
 
-#define N 1024
 #define MINUTES 1
 
 int main (void) {
     sleep (60 * MINUTES);
-    char *buffer = (char *) malloc (sizeof (char) * 8589934592);
+    char *buffer = (char *) malloc (sizeof (char) * INT_MAX);
     srand(time(NULL));
 
     time_t start_time = time(NULL);
 
     while (time(NULL) - start_time < MINUTES * 60) {
-        buffer[rand() % N];
+        int i = rand () % INT_MAX;
+    	char j = buffer[i];
     }
 
     return 0;
